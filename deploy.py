f37cd381418b668a160b07aea0312245ae8bc78a
Nathan <natethepcspecialist@gmail.com>
2015-12-06 19:38:19 -0500

    'update'

--- deploy.py
+++ deploy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os
-os.system("git add -u " +
+os.system("git add . && git add -u " +
 	"&& git commit -m 'update'" +
 	"&& git push origin master" +
 	"&& ssh -i al.pem ubuntu@ec2-54-165-181-175.compute-1.amazonaws.com 'python deploy-home.py'")
