3e935550280095b80321831f9b11bd720ce04cf3
Nathan <natethepcspecialist@gmail.com>
2015-12-11 22:19:18 -0500

    'update'

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $('.game').on('click', function () {
 	console.log(window.location.hostname + ":3000");
-	window.location = window.location.hostname + ":3000";
+	window.location = "http://" + window.location.hostname + ":3000";
 });
